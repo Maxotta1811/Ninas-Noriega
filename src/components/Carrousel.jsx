5c9dcdbdbee47d9fe3c80f7ba317210a5ada4562
Maxotta1811 <maxiotta_15@gmail.com>
2023-10-12 21:12:10 -0300

    carga de fotos en carrusel

--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -6,25 +6,28 @@ import Carrusel3 from '../images/carrusel/Carrusel3.jpg'
 function MyCarousel() {
   return (
     <Carousel>
-      <Carousel.Item style={{ width: '100%', height: '100vh' }}>
+      <Carousel.Item style={{ width: '100vw', height: 'auto' }}>
         <img
           className="imgcarousel"
           src={Carrusel1}
           alt="Primer slide"
+          style={{ width: '100%', height: '100%', objectFit: 'contain' }}
         />
       </Carousel.Item>
-      <Carousel.Item style={{ width: '100%', height: '100vh' }}>
+      <Carousel.Item style={{ width: '100vw', height: 'auto' }}>
         <img
           className="imgcarousel"
           src={Carrusel2}
           alt="Segundo slide"
+          style={{ width: '100%', height: '100%', objectFit: 'contain' }}
         />
       </Carousel.Item>
-      <Carousel.Item style={{ width: '100%', height: '100vh' }}>
+      <Carousel.Item style={{ width: '100vw', height: 'auto' }}>
         <img
           className="imgcarousel"
           src={Carrusel3}
           alt="Tercer slide"
+          style={{ width: '100%', height: '100%', objectFit: 'contain' }}
         />
       </Carousel.Item>
     </Carousel>
@@ -32,4 +35,4 @@ function MyCarousel() {
 }
 
 
-export default MyCarousel
+export default MyCarousel;
