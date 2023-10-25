d178521508ea6654095d62886a87c84e494b25f2
Maxotta1811 <maxiotta_15@gmail.com>
2023-10-24 23:04:15 -0300

    Pijamas

--- src/components/Pijamas.jsx
+++ src/components/Pijamas.jsx
@@ -1,8 +1,125 @@
 
 import { Container, Row, Col } from 'react-bootstrap';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import '../styles/Pijamas.css'
 
 const  Pijamas = () => {
+  const products = [
+    {
+      name: 'Adelma',
+      price: '$',
+      description: 'Musculosa con detalle de tul bordado y short con puntilla',
+      talle: "Talle: S, M, L, XL, XXL",
+      images: ['src/images/tienda/Pijamas/Adelma1.jpg',
+              'src/images/tienda/Pijamas/Adelma2.jpg'
+    ]
+
+    },
+    {
+      name: 'Antonia',
+      price: '$26.990',
+      description: 'Bralette de microtul con vuelo y short tiro alto en sedita',
+      talle: "Talle: S, M, L, XL, XXL",
+      images: ['src/images/tienda/Pijamas/Antonia.jpg',
+      'src/images/tienda/Pijamas/Antonia1.jpg'
+    ]
+    },
+    {
+      name: 'Bernardita',
+      price: '$29.990',
+      description: 'Remera con vuelos y short con puntilla',
+      talle: "Talle: S, M, L, XL, XXL",
+      images: ['src/images/tienda/Pijamas/Bernardita1.jpg',
+              'src/images/tienda/Pijamas/Bernardita2.jpg'
+            ]
+    },
+    {
+        name: 'Faustina',
+        price: '$26.990',
+        description: 'Top con srunchie y short tiro alto con vuelos',
+        talle: "Talle: S, M, L, XL, XXL",
+        images: ['src/images/tienda/Pijamas/Faustina1.jpg',
+                'src/images/tienda/Pijamas/Faustina2.jpg'
+              ]
+       
+      },
+      {
+        name: 'Horten',
+        price: '$28.990',
+        description: 'Musculosa con vuelos y breteles regulables, Short con vuelos ',
+        talle: "Talle: S, M, L, XL, XXL",
+        images: ['src/images/tienda/Pijamas/Horten1.jpg',
+        'src/images/tienda/Pijamas/Horten2.jpg'
+      ]
+        
+      },
+      {
+        name: 'Ines',
+        price: '$17.990',
+        description: 'Musculosa con breteles regulables y short con puntillas',
+        talle: "Talle: S, M, L, XL, XXL",
+        images: ['src/images/tienda/Pijamas/Ines1.jpg',
+        'src/images/tienda/Pijamas/Ines2.jpg']
+        
+      },
+      {
+        name: 'Isolina',
+        price: '$28.990',
+        description: 'Musculosa con breteles regulables y vuelo. Short con puntilla',
+        talle: "Talle: S, M, L, XL, XXL",
+        images: ['src/images/tienda/Pijamas/Isolina1.jpg',
+        'src/images/tienda/Pijamas/Isolina2.jpg']
+        
+      },
+      {
+        name: 'Merci',
+        price: '$24.990',
+        description: 'Musculosa con detalle de puntillas y vuelo. Short tiro alto',
+        talle: "Talle: S, M, L, XL, XXL",
+        images: ['src/images/tienda/Pijamas/Merci.jpg',
+        ]  
+      },
+      /*{
+        name: 'Horten',
+        price: '$28.990',
+        description: 'Musculosa con vuelos y breteles regulables, Short con vuelos ',
+        talle: "Talle: S, M, L, XL",
+        images: [ '../images/tienda/Pijamas/Horten1.jpg',
+        'src/images/tienda/Pijamas/Horten2.jpg']
+        
+      },
+      {
+        name: 'Ines',
+        price: '$17.990',
+        description: 'Musculosa con breteles regulables y short con puntillas',
+        talle: "Talle: S, M, L, XL",
+        images: ['../images/tienda/Pijamas/Ines1.jpg',
+        'src/images/tienda/Pijamas/Ines2.jpg']
+        
+      },
+      {
+        name: 'Isolina',
+        price: '$28.990',
+        description: 'Musculosa con breteles regulables y vuelo. Short con puntilla',
+        talle: "Talle: S, M, L, XL",
+        images: ['../images/tienda/Pijamas/Isolina1.jpg',
+        'src/images/tienda/Pijamas/Isolina2.jpg']
+        
+      },
+      {
+        name: 'Merci',
+        price: '$24.990',
+        description: 'Musculosa con detalle de puntillas y vuelo. Short tiro alto',
+        talle: "Talle: S, M, L, XL",
+        images: ['../images/tienda/Pijamas/Merci.jpg',
+        'src/images/tienda/Pijamas/Isolina2.jpg']
+        
+      },*/
+  ];
+
+/*const  Pijamas = () => {
   const products = [
     {
       name: 'ADELMA',
@@ -109,9 +226,16 @@ const  Pijamas = () => {
         talle: "Talle: S, M, L, XL, XXL",
         image: '../src/images/tienda/Pijamas/Antonia.jpg',
       },
-  ];
+  ];*/
+  const sliderSettings = {
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: true,
+  };
 
-  return (
+ return (
     <section className="gallery-section" >
       <Container>
 
@@ -119,8 +243,15 @@ const  Pijamas = () => {
           {products.map((product, index) => (
             <Col md={4} key={index}>
               <div className="product-item">
-                <img src={product.image} alt={product.name} />
-                <h5>{product.name}</h5>
+              <Slider {...sliderSettings}>
+                {product.images.map((image, imageIndex) => (
+                  <div key={imageIndex}>
+                    <img src={image} alt={`${product.name} - Image ${imageIndex + 1}`} />
+                  </div>
+                  ))}
+              </Slider>
+                
+                <h3>{product.name}</h3>
                 <p>{product.price}</p>
                 <p>{product.talle}</p>
                 <h2>{product.description}</h2>
@@ -132,5 +263,4 @@ const  Pijamas = () => {
     </section>
   );
 };
-
 export default Pijamas;
